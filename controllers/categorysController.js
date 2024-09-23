4f25f0c1298b131efd752fb8ab3a94a474c6200d
Adrián10-07 <adrian2aguilarg@gmail.com>
2024-09-23 16:41:28 -0600

    controller of categorys

--- controllers/categorysController.js
+++ controllers/categorysController.js
@@ -1,43 +1,42 @@
-const { Friend } = require('../models');
-
 
 const getAllCategory = async (req, res) => {
   try {
-    const friends = await Friend.findAll();
-    res.status(200).json(friends);
+    const categorys = await Category.findAll();
+    res.status(200).json(categorys);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
-const getFriendsById = async (req, res) => {
+const getCategoryById = async (req, res) => {
   try {
-    const friend = await Friend.findByPk(req.params.id);
-    if (friend) {
-      res.status(200).json(friend);
+    const categorys = await Category.findByPk(req.params.id);
+    if (categorys) {
+      res.status(200).json(categorys);
     } else {
-      res.status(404).json({ message: 'friend no encontrado' });
+      res.status(404).json({ message: 'category no encontrado' });
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-const deleteFriend = async (req, res) => {
+const deleteCategory = async (req, res) => {
   try {
-    const deleted = await Friend.destroy({
-      where: { id_friend: req.params.id }
+    const deleted = await Category.destroy({
+      where: { id_category: req.params.id }
     });
     if (deleted) {
-      res.status(204).json({ message: 'friend deleted' });
+      res.status(204).json({ message: 'category deleted' });
     } else {
-      res.status(404).json({ message: 'friend no encontrado' });
+      res.status(404).json({ message: 'category no encontrado' });
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 module.exports = {
-  getFriendsById,
-  getAllFriend,
-  deleteFriend
+getAllCategory,
+getCategoryById,
+deleteCategory
+
 };
